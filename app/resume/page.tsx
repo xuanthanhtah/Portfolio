2c266b8faf0bd3e8dc8c5c71ec573050db61ad08
Xuan Thanh <thanh26092000@gmail.com>
2024-08-29 10:12:29 +0700

    update

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -50,6 +50,64 @@ export default function Resume() {
     }
   };
 
+  const listSkills = [
+    {
+      title: 'Front-End',
+      subTitle: 'Developer',
+      skills: 'ReactJS, NextJS',
+      thumb: '/assets/image/fe.svg',
+    },
+    {
+      title: 'Back-End',
+      subTitle: 'Developer',
+      skills: 'C#',
+      thumb: '/assets/image/be.svg',
+    },
+    {
+      title: 'Database',
+      subTitle: 'Designer',
+      skills: 'SQL Server',
+      thumb: '/assets/image/uiux.svg',
+    },
+  ];
+
+  const listPort = [
+    {
+      title: 'Lorem...',
+      thumbBg: '/assets/image/circlepastel.svg',
+      thumb: '/assets/image/thumbportfolio.svg',
+    },
+    {
+      title: 'Lorem...',
+      thumbBg: '/assets/image/circlepastel.svg',
+      thumb: '/assets/image/thumbportfolio.svg',
+    },
+    {
+      title: 'Lorem...',
+      thumbBg: '/assets/image/circlepastel.svg',
+      thumb: '/assets/image/thumbportfolio.svg',
+    },
+  ];
+
+  const listContact = [
+    {
+      title: 'Github',
+      thumb: '/assets/image/github.svg',
+    },
+    {
+      title: 'Instagram',
+      thumb: '/assets/image/ins.svg',
+    },
+    {
+      title: 'Email',
+      thumb: '/assets/image/email.svg',
+    },
+    {
+      title: 'Phone',
+      thumb: '/assets/image/phone.svg',
+    },
+  ];
+
   return (
     <div className="relative">
       {isVisible && (
@@ -61,24 +119,26 @@ export default function Resume() {
         </button>
       )}
       {/* Backgroud */}
-      <div className="absolute top-[80px] left-[345px]">
-        <Image src="/assets/image/bg1.svg" width={200} height={190} alt="" />
-      </div>
-      <div className="absolute top-[444px] left-[545px]">
-        <Image src="/assets/image/bg1.svg" width={150} height={140} alt="" />
-      </div>
-      <div className="absolute top-[630px] left-[280px] z-0">
-        <Image src="/assets/image/bg1.svg" width={300} height={290} alt="" />
-      </div>
-      <div className="absolute top-[1000px] left-[980px] z-0">
-        <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
-      </div>
-      <div className="absolute top-[1270px] left-[220px] z-0">
-        <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
-      </div>
-      <div className="absolute top-[1570px] left-[1240px] z-0">
-        <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
-      </div>
+      <>
+        <div className="absolute top-[80px] left-[345px]">
+          <Image src="/assets/image/bg1.svg" width={200} height={190} alt="" />
+        </div>
+        <div className="absolute top-[444px] left-[545px]">
+          <Image src="/assets/image/bg1.svg" width={150} height={140} alt="" />
+        </div>
+        <div className="absolute top-[630px] left-[280px] z-0">
+          <Image src="/assets/image/bg1.svg" width={300} height={290} alt="" />
+        </div>
+        <div className="absolute top-[1000px] left-[980px] z-0">
+          <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
+        </div>
+        <div className="absolute top-[1270px] left-[220px] z-0">
+          <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
+        </div>
+        <div className="absolute top-[1570px] left-[1240px] z-0">
+          <Image src="/assets/image/bg2.svg" width={300} height={290} alt="" />
+        </div>
+      </>
       <div className="bg-white w-full h-full mx-auto">
         {/* Header */}
         <div className="pt-14 sticky top-0 z-40 bg-white">
@@ -173,7 +233,7 @@ export default function Resume() {
           </div>
         </div>
         {/*Additional passions */}
-        <div className=" px-80" id="passions">
+        <div className=" px-80 pt-[170px]" id="passions">
           <div className="font-poppins w-full text-center text-[35px] leading-[normal] tracking-[0.96px] pb-[60px]">
             <span className="text-center text-indigo-800">{'Additional '}</span>
             <span className="text-center font-semibold text-blue-950">
@@ -181,72 +241,35 @@ export default function Resume() {
             </span>
           </div>
           <div className="flex justify-between">
-            <div className="z-10 w-[347px] h-[280px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)]">
-              <div className="px-[61px] py-5">
-                <Image
-                  src={'/assets/image/fe.svg'}
-                  alt={''}
-                  width={225}
-                  height={159}
-                />
-              </div>
-              <div className="text-center">
-                <span className="text-black font-poppins text-[25px] font-normal leading-normal tracking-[1.375px]">
-                  <span className="text-black text-center font-poppins text-[25px] font-semibold leading-normal tracking-[1.375px]">
-                    Front-End
-                  </span>{' '}
-                  Developer
-                </span>
-                <br />
-                <span className="text-black text-center font-poppins text-[18px] font-normal leading-normal tracking-[0.99px]">
-                  ReactJS, NextJS
-                </span>
-              </div>
-            </div>
-            <div className="z-10 w-[347px] h-[280px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)]">
-              <div className="px-[61px] py-5">
-                <Image
-                  src={'/assets/image/be.svg'}
-                  alt={''}
-                  width={225}
-                  height={159}
-                />
-              </div>
-              <div className="text-center">
-                <span className="text-black font-poppins text-[25px] font-normal leading-normal tracking-[1.375px]">
-                  <span className="text-black text-center font-poppins text-[25px] font-semibold leading-normal tracking-[1.375px]">
-                    Back-End
-                  </span>{' '}
-                  Developer
-                </span>
-                <br />
-                <span className="text-black text-center font-poppins text-[18px] font-normal leading-normal tracking-[0.99px]">
-                  C#
-                </span>
-              </div>
-            </div>
-            <div className="z-10 w-[347px] h-[280px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)]">
-              <div className="px-[61px] py-5">
-                <Image
-                  src={'/assets/image/uiux.svg'}
-                  alt={''}
-                  width={225}
-                  height={159}
-                />
-              </div>
-              <div className="text-center">
-                <span className="text-black font-poppins text-[25px] font-normal leading-normal tracking-[1.375px]">
-                  <span className="text-black text-center font-poppins text-[25px] font-semibold leading-normal tracking-[1.375px]">
-                    Database
-                  </span>{' '}
-                  Designer
-                </span>
-                <br />
-                <span className="text-black text-center font-poppins text-[18px] font-normal leading-normal tracking-[0.99px]">
-                  SQL Server
-                </span>
-              </div>
-            </div>
+            {listSkills.length > 0
+              ? listSkills.map((skill, index) => (
+                  <div
+                    key={index}
+                    className="z-10 xl3:w-[285px] xl3:h-[220px] xl4:w-[285px] xl4:h-[220px] xl5:w-[310px] xl5:h-[250px] w-[347px] h-[280px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)]"
+                  >
+                    <div className="px-[61px] py-5">
+                      <Image
+                        src={skill.thumb}
+                        alt={`${skill.title} ${skill.subTitle}`}
+                        width={225}
+                        height={159}
+                      />
+                    </div>
+                    <div className="text-center">
+                      <span className="text-black font-poppins xl3:leading-[0.8] xl3:text-[18px] xl3:tracking-[0.375px] xl4:text-[20px] xl4:tracking-[0.375px] xl5:text-[22px] text-[25px] font-normal leading-normal tracking-[1.375px]">
+                        <span className="text-black text-center font-poppins xl3:leading-[0.8] xl3:text-[18px] xl3:tracking-[0.375px] xl4:text-[20px] xl4:tracking-[0.375px] xl5:text-[22px] text-[25px] font-semibold leading-normal tracking-[1.375px]">
+                          {skill.title}
+                        </span>{' '}
+                        {skill.subTitle}
+                      </span>
+                      <br />
+                      <span className="text-black text-center font-poppins text-[18px] font-normal leading-normal tracking-[0.99px]">
+                        {skill.skills}
+                      </span>
+                    </div>
+                  </div>
+                ))
+              : null}
           </div>
         </div>
         {/* Portfolio */}
@@ -258,75 +281,28 @@ export default function Resume() {
             </span>
           </div>
           <div className="flex justify-between">
-            <div className="w-[347px] h-[266px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)] relative">
-              <div className="opacity-70 absolute left-[17px] top-[19px]">
-                <Image
-                  src="/assets/image/circlepastel.svg"
-                  alt=""
-                  width={195}
-                  height={195}
-                />
-              </div>
-              <div className="absolute left-[18px] top-[26px] flex flex-col">
-                <span className="text-[#4581F6] font-montserrat text-[25px] font-semibold leading-normal">
-                  Lorem...
-                </span>
-              </div>
-              <div className="absolute right-3 bottom-4">
-                <Image
-                  src="/assets/image/thumbportfolio.svg"
-                  alt=""
-                  width={289}
-                  height={164}
-                />
-              </div>
-            </div>
-            <div className="w-[347px] h-[266px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)] relative">
-              <div className="opacity-70 absolute left-[17px] top-[19px]">
-                <Image
-                  src="/assets/image/circlepastel.svg"
-                  alt=""
-                  width={195}
-                  height={195}
-                />
-              </div>
-              <div className="absolute left-[18px] top-[26px] flex flex-col">
-                <span className="text-[#4581F6] font-montserrat text-[25px] font-semibold leading-normal">
-                  Lorem...
-                </span>
-              </div>
-              <div className="absolute right-3 bottom-4">
-                <Image
-                  src="/assets/image/thumbportfolio.svg"
-                  alt=""
-                  width={289}
-                  height={164}
-                />
-              </div>
-            </div>
-            <div className="w-[347px] h-[266px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)] relative">
-              <div className="opacity-70 absolute left-[17px] top-[19px]">
-                <Image
-                  src="/assets/image/circlepastel.svg"
-                  alt=""
-                  width={195}
-                  height={195}
-                />
-              </div>
-              <div className="absolute left-[18px] top-[26px] flex flex-col">
-                <span className="text-[#4581F6] font-montserrat text-[25px] font-semibold leading-normal">
-                  Lorem...
-                </span>
-              </div>
-              <div className="absolute right-3 bottom-4">
-                <Image
-                  src="/assets/image/thumbportfolio.svg"
-                  alt=""
-                  width={289}
-                  height={164}
-                />
-              </div>
-            </div>
+            {listPort.length > 0
+              ? listPort.map((item, index) => (
+                  <div className="w-[347px] h-[266px] flex-shrink-0 rounded-[13px] bg-white shadow-[0px_5px_26px_5px_rgba(0,_0,_0,_0.17)] relative">
+                    <div className="opacity-70 absolute left-[17px] top-[19px]">
+                      <Image
+                        src={item.thumbBg}
+                        alt={item.title}
+                        width={195}
+                        height={195}
+                      />
+                    </div>
+                    <div className="absolute left-[18px] top-[26px] flex flex-col">
+                      <span className="text-[#4581F6] font-montserrat text-[25px] font-semibold leading-normal">
+                        {item.title}
+                      </span>
+                    </div>
+                    <div className="absolute right-3 bottom-4">
+                      <Image src={item.thumb} alt="" width={289} height={164} />
+                    </div>
+                  </div>
+                ))
+              : null}
           </div>
         </div>
         {/* Contact Me */}
@@ -338,118 +314,38 @@ export default function Resume() {
           </div>
 
           <div className="flex justify-between">
-            <div
-              className="w-[240px] h-[236px] flex-shrink-0 textAlignCenter cursor-pointer"
-              onClick={() => {
-                handleClick('github');
-              }}
-            >
-              <div className="relative">
-                <Image
-                  src="/assets/image/vector.svg"
-                  alt=""
-                  width={166}
-                  height={160}
-                />
-                <div className="absolute top-[20%] left-[29%]">
-                  <Image
-                    src="/assets/image/github.svg"
-                    alt=""
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-              <div>
-                <span className="text-[#3F3D56] text-center font-poppins text-[35px] font-medium leading-normal tracking-[1.925px]">
-                  Github
-                </span>
-              </div>
-            </div>
-            <div
-              className="w-[240px] h-[236px] flex-shrink-0 textAlignCenter cursor-pointer"
-              onClick={() => {
-                handleClick('instagram');
-              }}
-            >
-              <div className="relative">
-                <Image
-                  src="/assets/image/vector.svg"
-                  alt=""
-                  width={166}
-                  height={160}
-                />
-                <div className="absolute top-[20%] left-[29%]">
-                  <Image
-                    src="/assets/image/ins.svg"
-                    alt=""
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-              <div>
-                <span className="text-[#3F3D56] text-center font-poppins text-[35px] font-medium leading-normal tracking-[1.925px]">
-                  Instagram
-                </span>
-              </div>
-            </div>
-            <div
-              className="w-[240px] h-[236px] flex-shrink-0 textAlignCenter cursor-pointer"
-              onClick={() => {
-                handleClick('email');
-              }}
-            >
-              <div className="relative">
-                <Image
-                  src="/assets/image/vector.svg"
-                  alt=""
-                  width={166}
-                  height={160}
-                />
-                <div className="absolute top-[20%] left-[29%]">
-                  <Image
-                    src="/assets/image/email.svg"
-                    alt=""
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-              <div>
-                <span className="text-[#3F3D56] text-center font-poppins text-[35px] font-medium leading-normal tracking-[1.925px]">
-                  Email
-                </span>
-              </div>
-            </div>
-            <div
-              className="w-[240px] h-[236px] flex-shrink-0 textAlignCenter cursor-pointer"
-              onClick={() => {
-                handleClick('phone');
-              }}
-            >
-              <div className="relative">
-                <Image
-                  src="/assets/image/vector.svg"
-                  alt=""
-                  width={166}
-                  height={160}
-                />
-                <div className="absolute top-[20%] left-[29%]">
-                  <Image
-                    src="/assets/image/phone.svg"
-                    alt=""
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-              <div>
-                <span className="text-[#3F3D56] text-center font-poppins text-[35px] font-medium leading-normal tracking-[1.925px]">
-                  Phone
-                </span>
-              </div>
-            </div>
+            {listContact.length > 0
+              ? listContact.map((item, index) => (
+                  <div
+                    className="w-[240px] h-[236px] flex-shrink-0 textAlignCenter cursor-pointer"
+                    onClick={() => {
+                      handleClick(`${item.title}`);
+                    }}
+                  >
+                    <div className="relative">
+                      <Image
+                        src="/assets/image/vector.svg"
+                        alt=""
+                        width={166}
+                        height={160}
+                      />
+                      <div className="absolute top-[20%] left-[29%]">
+                        <Image
+                          src={item.thumb}
+                          alt=""
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <span className="text-[#3F3D56] text-center font-poppins text-[35px] font-medium leading-normal tracking-[1.925px]">
+                        {item.title}
+                      </span>
+                    </div>
+                  </div>
+                ))
+              : null}
           </div>
         </div>
       </div>
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,8 +1,8 @@
 const helper = {
-  github: 'https://github.com/xuanthanhtah',
-  instagram: 'https://www.instagram.com/x.thanhhhhhhh/',
-  email: 'mailto:thanh26092000@gmail.com',
-  phone: 'tel:0399745742',
+  Github: 'https://github.com/xuanthanhtah',
+  Instagram: 'https://www.instagram.com/x.thanhhhhhhh/',
+  Email: 'mailto:thanh26092000@gmail.com',
+  Phone: 'tel:0399745742',
 };
 
 export default helper;
